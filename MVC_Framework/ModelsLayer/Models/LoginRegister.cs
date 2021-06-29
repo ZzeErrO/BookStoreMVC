03d2ee8cf52fefeb0bf65c1d28fca506f04caf32
prashikjaware <prashikjaware123@gmail.com>
2021-06-29 19:06:57 +0530

    [Prashik]Added: Added Controller and View

---
+++ MVC_Framework/ModelsLayer/Models/LoginRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLayer.Models
+{
+    public class LoginRegister
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [RegularExpression("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "E-mail is not valid. Please Entet valid email")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Invalid Password")]
+        public string Password { get; set; }
+    }
+}
